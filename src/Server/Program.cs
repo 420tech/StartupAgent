4112459b2bad446623c12ab9ed47c352b7d1ebbd
420Tech <timbelvin@hotmail.com>
2026-01-19 17:15:42 -0800

    Initial: Blazor Web App with WebAssembly interactivity, module structure, and test projects - all projects build successfully

---
+++ src/Server/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
